8715666227234916604847a7931ad98fa2596330
Hero <76834176+jish0101@users.noreply.github.com>
2024-02-26 22:55:37 +0530

    update

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,25 +1,13 @@
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Loader from './Loader';
 import AppLayout from '../AppLayout/AppLayout';
-import Footer from '../Footer/Footer';
-import { LoadingOverlay, ScrollArea } from '@mantine/core';
+import { LoadingOverlay } from '@mantine/core';
 import { useSelector } from 'react-redux';
 import { selectIsLoading } from '../../Store/reducers/globalLoader/loaderSelector';
 
 const Layout = () => {
   const isLoading = useSelector(selectIsLoading);
-  // const scrollRef = useRef();
-
-  // const scrollTop = () => {
-  //   try {
-  //     if (scrollRef && scrollRef.current) {
-  //       scrollRef.current.scrollTo({ top: 0, behavior: 'smooth' });
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
 
   return (
     <Suspense fallback={<Loader />}>
